1258d9957af0a4fa39bad1935b2873bb8d137859
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-02-12 19:07:29 -0700

    Temporarily changing home to default HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/screens/page_1/landing.g.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'My Home',
+      ),
     );
   }
 }
