6a666802a478648031ecfe1dee5451ab28103ffd
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-02-12 20:05:39 -0700

    Add animated text

--- lib/controller/tag/software_developer_custom.dart
+++ lib/controller/tag/software_developer_custom.dart
@@ -1,3 +1,5 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class SoftwareDeveloperCustom extends StatefulWidget {
@@ -13,9 +15,43 @@ class SoftwareDeveloperCustom extends StatefulWidget {
       _SoftwareDeveloperCustomState();
 }
 
-class _SoftwareDeveloperCustomState extends State<SoftwareDeveloperCustom> {
+class _SoftwareDeveloperCustomState extends State<SoftwareDeveloperCustom>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 350,
+      ),
+    );
+    _animationController.repeat(reverse: true);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return widget.child!;
+    return Row(
+      children: [
+        DefaultTextStyle(
+          style: const TextStyle(color: Colors.green, fontSize: 26),
+          child: AnimatedTextKit(
+            animatedTexts: [
+              TypewriterAnimatedText('Software Developer'),
+              TypewriterAnimatedText('Movie Enthusiast'),
+              TypewriterAnimatedText('Gamer')
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
