0b196594123133f25c7bb755f686a6b0d561789b
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-02-19 22:35:09 -0700

    Removed unnecessary logic and widgets from SoftwareDeveloperCustom.

--- lib/controller/tag/software_developer_custom.dart
+++ lib/controller/tag/software_developer_custom.dart
@@ -1,5 +1,4 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class SoftwareDeveloperCustom extends StatefulWidget {
@@ -15,44 +14,20 @@ class SoftwareDeveloperCustom extends StatefulWidget {
       _SoftwareDeveloperCustomState();
 }
 
-class _SoftwareDeveloperCustomState extends State<SoftwareDeveloperCustom>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-
-  @override
-  void initState() {
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(
-        milliseconds: 350,
-      ),
-    );
-    _animationController.repeat(reverse: true);
-    super.initState();
-  }
-
+class _SoftwareDeveloperCustomState extends State<SoftwareDeveloperCustom> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        DefaultTextStyle(
-          style: const TextStyle(color: Colors.green, fontSize: 26),
-          child: AnimatedTextKit(
-            animatedTexts: [
-              TypewriterAnimatedText('Software Developer'),
-              TypewriterAnimatedText('Movie Enthusiast'),
-              TypewriterAnimatedText('Gamer 😎')
-            ],
-            repeatForever: true,
-          ),
-        ),
-      ],
+    // return widget.child!;
+    return DefaultTextStyle(
+      style: const TextStyle(color: Colors.green, fontSize: 26),
+      child: AnimatedTextKit(
+        animatedTexts: [
+          TypewriterAnimatedText('Software Developer'),
+          TypewriterAnimatedText('Movie Enthusiast'),
+          TypewriterAnimatedText('Gamer 😎')
+        ],
+        repeatForever: true,
+      ),
     );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
   }
 }
