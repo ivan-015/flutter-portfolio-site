2371c8bb0075c9439bbf1844fb75ae773cf8889c
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-03-28 18:38:02 -0600

    Add animated position for bottom arrow

--- lib/controller/tag/down_arrow_custom.dart
+++ lib/controller/tag/down_arrow_custom.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class DownArrowCustom extends StatefulWidget {
   final Widget? child;
 
-  DownArrowCustom({
+  const DownArrowCustom({
     Key? key,
     this.child,
   }) : super(key: key);
@@ -13,8 +15,43 @@ class DownArrowCustom extends StatefulWidget {
 }
 
 class _DownArrowCustomState extends State<DownArrowCustom> {
+  bool _update = true;
+  late Timer _timer;
+
+  /// Duration of the animation and how often it should change positions.
+  final duration = const Duration(milliseconds: 700);
+
+  @override
+  void initState() {
+    _timer = Timer.periodic(
+      duration,
+      (timer) {
+        setState(() => _update = !_update);
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return widget.child!;
+    return Stack(
+      children: [
+        AnimatedPositioned(
+          width: 60,
+          height: 80,
+          bottom: _update ? 0 : 25,
+          left: 0,
+          child: widget.child!,
+          duration: duration,
+        ),
+      ],
+    );
+    // return widget.child!;
   }
 }
